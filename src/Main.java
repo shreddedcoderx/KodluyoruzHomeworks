27d1db77ddbbfa0101f06dfcd45a0441986f0df5
shreddedcoderx <shreddedcoderx@gmail.com>
2022-07-10 23:43:55 +0300

    while loop codes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int pass = 123;
+        int enteredPass;
+        boolean isPasswordSuccess = false;
+        Scanner scanner = new Scanner(System.in);
+
+        while (!isPasswordSuccess) {
+            System.out.print("Şifrenizi girin: ");
+            enteredPass = scanner.nextInt();
+            if (enteredPass == pass) {
+                isPasswordSuccess = true;
+                System.out.println("Şifre doğru.Giriş yapıldı.");
+
+            } else {
+                System.out.println("Şifre Yanlış.Farklı bir şifre deneyin.");
+            }
+        }
+
+        scanner.close();
     }
 }
