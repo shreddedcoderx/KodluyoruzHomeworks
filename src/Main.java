cf0f5a0e77c4591bcfdc85551c300eff696a225b
shreddedcoderx <shreddedcoderx@gmail.com>
2022-07-10 23:59:56 +0300

    do-while loop codes 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int pass;
+        boolean askPass = true;
+
+
+        do {
+            System.out.print("Lütfen şifre giriniz: ");
+            pass = scanner.nextInt();
+            if (pass == 123){
+                askPass = false;
+                System.out.println("Şifre doğru.");
+            } else {
+                System.out.println("Şifre yanlış.");
+            }
+        } while (askPass);
+
+
     }
 }
