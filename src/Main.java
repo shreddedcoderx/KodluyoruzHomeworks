2958610b941ba2b406fde93da8eefebc3bdc436d
shreddedcoderx <shreddedcoderx@gmail.com>
2022-07-10 23:48:35 +0300

    while loop codes 2

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int pass = 123;
-        int enteredPass;
-        boolean isPasswordSuccess = false;
-        Scanner scanner = new Scanner(System.in);
+        int left = 100, right = 200;
 
-        while (!isPasswordSuccess) {
-            System.out.print("Şifrenizi girin: ");
-            enteredPass = scanner.nextInt();
-            if (enteredPass == pass) {
-                isPasswordSuccess = true;
-                System.out.println("Şifre doğru.Giriş yapıldı.");
+        while (++left < --right);
 
-            } else {
-                System.out.println("Şifre Yanlış.Farklı bir şifre deneyin.");
-            }
-        }
-
-        scanner.close();
+        System.out.println("Left : " + left);
+        System.out.println("Right : " + right);
     }
 }
