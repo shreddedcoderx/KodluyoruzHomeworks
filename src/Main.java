77bd0a4a1f1668c258680c4726e526246748673b
shreddedcoderx <shreddedcoderx@gmail.com>
2022-07-11 00:55:46 +0300

    for loop codes 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sayi;
+/*
+        for (boolean run = true; run;) {
+            System.out.print("Sayi giriniz : ");
+            sayi = scanner.nextInt();
+            if (sayi < 0) {
+                run = false;
+            }
+        }*/
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
+        do {
+            System.out.print("Sayi giriniz : ");
+            sayi = scanner.nextInt();
+        } while (sayi > 0);
+
+
     }
 }
